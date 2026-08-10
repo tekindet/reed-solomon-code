5af9f795fca149bbb4ec6ee020a12369c05c21b7
aitumik <aitumik@protonmail.com>
2026-08-10 13:54:16 +0300

    wip: implement berlekamp_massey algorithm

--- main.py
+++ main.py
@@ -12,6 +12,88 @@ class RSCodec:
         self.nsym = 4
         self.n = n
 
+
+    def berlekamp_massey_second(self, syndromes):
+        # C(x) starts as 1
+        C = GF256Poly([1])
+        B = GF256Poly([1])
+        L = 0
+        m = 1
+
+        for n in range(len(syndromes)):
+            # Compute discrepancy d
+            d = syndromes[n]
+            for i in range(1, L + 1):
+                if i <= len(C.coeffs) - 1:
+                    coef = C.coeffs[-(i + 1)]
+                    d = GF256.add(d, GF256.mul(coef, syndromes[n - i]))
+
+            if d == 0:
+                m += 1
+            else:
+                T = C
+                # C(x) = C(x) + d * B(x) * x^m
+                # Construct (d * B(x) * x^m)
+                scaled_B_coeffs = [GF256.mul(d, c) for c in B.coeffs] + [0] * m
+                scaled_B = GF256Poly(scaled_B_coeffs)
+                C = C + scaled_B  # Polynomial addition in GF(2^8) is XOR
+
+                if 2 * L <= n:
+                    L = n + 1 - L
+                    # B = T / d
+                    d_inv = GF256.inv(d)
+                    B = GF256Poly([GF256.mul(c, d_inv) for c in T.coeffs])
+                    m = 1
+                else:
+                    m += 1
+
+        return C
+
+    def berlekamp_massey(self,synds):
+
+        C = [1]
+        B = [1]
+
+        v = len(synds)
+
+        for N in range(v):
+            d = synds[N]
+
+            for i in range(1,len(C)):
+                d = GF256.add(d, GF256.mul(C[i], syndromes[N - i]))
+
+                #t_poly = GF256Poly([C[i]]) * syndromes[N - i]
+
+                #d = d + t_poly
+
+
+            B = [0] + B
+
+            if d != 0:
+                T = list(C)
+
+                len_diff = abs(len(B) - len(C))
+
+                c1 = C + [0] * (len(B) - len(C)) if len(C) < len(B) else C
+                c2 = B + [0] * (len(C) - len(B)) if len(B) < len(C) else B
+
+                C = [GF256.add(a, GF256.mul(1, b)) for a, b in zip(c1, c2)]
+
+                if 2 * (len(T) - 1) <= N:
+
+                    #u,r = GF256Poly(T).divmod(d)
+                    #B = u.coeffs
+
+                    #quotient,remainder = d.divmod(GF256Poly(T))
+                    B = [GF256.div(x, d) for x in T]
+                    #B = quotient.coeffs
+                    #B = [GF256Poly(T).divmod(d)[0].coeffs[0] for x in T]
+
+                    print(B)
+
+        return GF256Poly(C)
+
+
     def encode(self,msg):
 
         msg_bytes = [ord(c) for c in msg]
@@ -53,16 +135,38 @@ class RSCodec:
 
             Q_x,P_x = R_x.divmod(factor)
 
-            rems.append(P_x)
+            rem_val = P_x.coeffs[0] if P_x.coeffs else 0
+            rems.append(rem_val)
 
 
         return rems
 
-    def correct_error_locations(self):
+    def correct_errors(self,coeffs):
         """
         Can only correct upto t errors where t = (n - k) / 2
         """
-        pass
+        synds = self.decode(coeffs)
+
+        largest = 0
+
+        R_x = GF256Poly(synds)
+
+        #largest = max(largest,c.coeffs[0]) for c in R_x.coeffs
+        largest = [max()for c in R_x.coeffs]
+
+        """
+        for i in R_x.coeffs:
+            c = i.coeffs[0]
+            largest = max(largest,c)
+
+        """
+
+
+        if largest == 0:
+            print("Syndromes all ZERO -> No errors detected")
+            return coeffs
+        else:
+            print("There are some errors you need to deal with")
 
 
     def find_error_locations(self,synds):
@@ -71,26 +175,41 @@ class RSCodec:
         """
 
         # Step 1 : Find the error locator polynomial
-        E_x = GF256Poly([1])
-
+        A_x = GF256Poly([1])
         v = len(synds)
 
+        t = len(synds)
+
+        for i in range(t - 1):
+            alpha_i = GF256.pow(2,i)
+
+            factor = GF256Poly([1,alpha_i])
+
+            #print(factor * synds[i])
+
+            A_x *= factor
+
+        """
         for i in range(v):
             d = synds[i]
+            X_i = GF256.pow(2,i)
 
-            print(d)
+            factor = GF256Poly([1,X_i])
 
-            #alpha_j = GF256.pow(2,j)
 
-            #factor = GF256Poly([1,alpha_j])
-
-            #E_x *= factor
+            A_x *= factor
 
         # Step 2 : Find the error locations(Chien search)
+        for i in range(v):
+            continue
 
         # Step 3 : Calculate the error magnitudes(Forney Algo)
+        for i in range(v):
+            continue
 
-        return E_x
+        """
+
+        return A_x
 
 
 def build_generator_poly(nsym):
@@ -132,6 +251,11 @@ class GF256:
         return cls.exp[cls.log[a] + cls.log[b]]
 
     @classmethod
+    def inv(cls, a):
+        if a == 0: raise ZeroDivisionError("multiplicative inverse of zero is undefined in galois field")
+        return cls.exp[255 - cls.log[a]]
+
+    @classmethod
     def add(cls,a,b):
         return a ^ b
 
@@ -157,6 +281,17 @@ class GF256Poly:
         while i < len(coeffs) - 1 and coeffs[i] == 0:
             i = i + 1
         self.coeffs = coeffs[i:]
+
+    def eval(self,x_val):
+
+        res = 0
+
+        for c in range(self.coeffs):
+
+            res = GF256.add(GF256.mul(res,x_val),c)
+
+        return res
+
 
     def __add__(self,other):
         len_diff = abs(len(self.coeffs) - len(other.coeffs))
@@ -232,25 +367,28 @@ class GF256Poly:
         return " + ".join(terms) if terms else "0"
 
 if __name__ == "__main__":
-    msg = "this is a test message"
+    msg = "This is a test message"
 
     kodek = RSCodec(8,4)
 
     C_x = kodek.encode(msg)
 
-    # todo : need to corrupt the message to see if it works
-
     encoded_msg = bytes(C_x.coeffs)
 
     corrupted_msg = bytearray(encoded_msg)
-    #corrupted_msg[0] ^= 0xFF
+    corrupted_msg[0] ^= 0xFF
 
-    rems = kodek.decode(corrupted_msg)
-    #print(rems)
+    syndromes = kodek.decode(corrupted_msg)
+
+    res = kodek.berlekamp_massey(syndromes)
+    print(res)
 
 
-    res = kodek.find_error_locations(rems)
-    #print(res)
+
+
+
+
+
 
 
 
